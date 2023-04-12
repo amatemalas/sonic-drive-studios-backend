70fec2354aed3b18b9a90539fd67f186b464c208
Adrián Matemalas <amatemalas@gmail.com>
2023-04-12 22:20:03 +0200

    Add some bottom space on work cards

--- resources/views/frontend/partials/works/list.blade.php
+++ resources/views/frontend/partials/works/list.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     @foreach ($items as $item)
-        <div class="col-lg-4 col-md-6">
+        <div class="col-lg-4 col-md-6 mb-4">
             @include('frontend.partials.works.item')
         </div>
     @endforeach
